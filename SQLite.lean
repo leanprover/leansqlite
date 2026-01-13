59489e23ef72c0a44f3b29b4fb018209a841f498
David Thrane Christiansen <david@davidchristiansen.dk>
2026-01-13 13:19:18 +0100

    feat: docs and better interpolation

--- SQLite.lean
+++ SQLite.lean
@@ -4,18 +4,22 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author: David Thrane Christiansen
 -/
 module
--- This module serves as the root of the `Leansqlite` library.
--- Import modules here that should be built as part of the library.
 public import SQLite.FFI
 public import SQLite.LowLevel
 import Std.Data.Iterators
+
+set_option doc.verso true
+set_option linter.missingDocs true
 
 namespace SQLite
 
 section Interpolation
 open Lean
 
-public class ToSQLite α where
+/--
+Provides a canonical means of binding a value of type {name}`α` in a SQLite query.
+-/
+public class ToSQLite (α : Type u) where
   bind : Stmt → Int32 → α → IO Unit
 
 public instance : ToSQLite String where
@@ -25,13 +29,13 @@ public instance : ToSQLite ByteArray where
   bind := Stmt.bindBlob
 
 public instance : ToSQLite Int32 where
-  bind := Stmt.bindInt
+  bind := Stmt.bindInt32
 
 public instance : ToSQLite Int64 where
   bind := Stmt.bindInt64
 
 public instance : ToSQLite Float where
-  bind := Stmt.bindDouble
+  bind := Stmt.bindFloat
 
 public instance : ToSQLite Unit where
   bind stmt index _ := Stmt.bindNull stmt index
@@ -39,33 +43,13 @@ public instance : ToSQLite Unit where
 public instance : ToSQLite Nat where
   bind stmt i n := Stmt.bindText stmt i (toString n)
 
-syntax term "sql!" interpolatedStr(term) : term
-
-macro_rules
-  | `($db sql!%$tk $s) => do
-    let chunks := s.raw.getArgs
-    let mut sqlStx ← `("")
-    let mut args := #[]
-    let stmtName := mkIdentFrom tk `stmt
-    for c in chunks do
-      if let some lit := c.isInterpolatedStrLit? then
-        sqlStx ← `($sqlStx ++ $(quote lit))
-      else
-        let index := args.size + 1
-        args := args.push (← ``(ToSQLite.bind $stmtName (Nat.toInt32 $(quote index)) $(⟨c⟩)))
-        sqlStx ← `($sqlStx ++ $(quote s!"?{index}"))
-
-    let stx ← `(((let db := $db; SQLite.prepare db $sqlStx >>= fun $stmtName =>
-        (([$(args),*] : List (IO Unit)).forM id) *>
-        pure $stmtName) : IO Stmt))
-
-    return stx
-
 end Interpolation
 
-
-public class Field α where
-  get : Stmt → Int32 → IO α
+/--
+Provides a canonical way to read a value of type {name}`α` from a SQL query.
+-/
+public class Field (α : Type) where
+  get (stmt : Stmt) (column : Int32) : IO α
 
 -- Reading a field can trigger type conversion, so this pattern is not generally safe. Null stays
 -- null, at least, so this one is OK.
@@ -93,15 +77,26 @@ public instance : Field Int64 where
 public instance : Field Nat where
   get stmt i := (·.toNatClampNeg) <$> Stmt.columnInt64 stmt i
 
-public abbrev RowReader α := ReaderT Stmt (StateRefT Int32 IO) α
+/--
+A monad for reading a row of data from a query result.
+-/
+public abbrev RowReader (α : Type) := ReaderT Stmt (StateRefT Int32 IO) α
 
 namespace RowReader
 
+/--
+Reads the next column using the {lean}`Field α` instance, updating the column counter.
+-/
 public def field [Field α] : RowReader α := do
   let v ← Field.get (← read) (← getThe Int32)
   modify (· + 1)
   return v
 
+/--
+Runs a row reader on a prepared statement.
+
+The statement should have just been stepped, and returned {lean}`true`.
+-/
 public def run (stmt : Stmt) (act : RowReader α) : IO α := do
   -- The initial column index is 0 here. Binding parameters is 1-indexed, reading columns is
   -- 0-indexed in SQLite.
@@ -109,7 +104,10 @@ public def run (stmt : Stmt) (act : RowReader α) : IO α := do
 
 end RowReader
 
-public class Row α where
+/--
+A type that can be deserialized from a complete row in a SQL query.
+-/
+public class Row (α : Type) where
   read : RowReader α
 
 public instance [Field α] : Row α where
@@ -118,7 +116,10 @@ public instance [Field α] : Row α where
 public instance [Row α] [Row β] : Row (α × β) where
   read := do return (← Row.read, ← Row.read)
 
-public structure QueryIterator β where
+/--
+An iterator over query results. Stepping the iterator steps the underlying prepared statement.
+-/
+public structure QueryIterator (β : Type) where
   [isRow : Row β]
   stmt : Stmt
 
@@ -140,7 +141,57 @@ public instance : Iterator (QueryIterator β) IO β where
 
 public instance [Monad n] : IteratorLoop (QueryIterator β) IO n := IteratorLoop.defaultImplementation
 
+/--
+Returns an iterator into all the results of a prepared statement.
+-/
 public def Stmt.results [Row α] (stmt : Stmt) : @IterM (QueryIterator α) IO α where
   internalState := QueryIterator.mk stmt
 
+/--
+Returns an iterator into all the results of a prepared statement, with the type specified explicitly.
+-/
 public def Stmt.resultsAs (α : Type) [Row α] (stmt : Stmt) : @IterM (QueryIterator α) IO α := stmt.results
+
+namespace Interpolation
+open Lean
+
+/--
+An interpolated query that creates a prepared statement.
+-/
+syntax term "sql!" interpolatedStr(term) : term
+
+/--
+An interpolated query is run via {name}`SQLite.exec`, discarding the results.
+-/
+syntax term "exec!" interpolatedStr(term) : term
+
+/--
+An interpolated query that creates an iterator into the results.
+-/
+syntax term "query!" interpolatedStr(term) (&" as " term)? : term
+
+macro_rules
+  | `($db sql!%$tk $s) => do
+    let chunks := s.raw.getArgs
+    let mut sqlStx ← `("")
+    let mut args := #[]
+    let stmtName := mkIdentFrom tk `stmt
+    for c in chunks do
+      if let some lit := c.isInterpolatedStrLit? then
+        sqlStx ← `($sqlStx ++ $(quote lit))
+      else
+        let index := args.size + 1
+        args := args.push (← ``(ToSQLite.bind $stmtName (Nat.toInt32 $(quote index)) $(⟨c⟩)))
+        sqlStx ← `($sqlStx ++ $(quote s!"?{index}"))
+
+    let stx ← `(((let db := $db; SQLite.prepare db $sqlStx >>= fun $stmtName =>
+        (([$(args),*] : List (IO Unit)).forM id) *>
+        return $stmtName) : IO Stmt))
+
+    return stx
+  | `($db exec!%$tk $s) => do
+    `(($db sql!%$tk $s >>= Stmt.exec : IO Unit))
+  | `($db query!%$tk $s) => do
+    `($db sql!%$tk $s <&> Stmt.results)
+  | `($db query!%$tk $s as $ty) => do
+    `($db sql!%$tk $s <&> Stmt.resultsAs $ty)
