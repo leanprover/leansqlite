eb5513cc0bb10842adf1dd62c1c4558084201c0f
David Thrane Christiansen <david@davidchristiansen.dk>
2026-01-14 10:15:13 +0100

    fix: control nullability (#5)

--- SQLite.lean
+++ SQLite.lean
@@ -18,65 +18,94 @@ section Interpolation
 open Lean
 
 /--
-Provides a canonical means of binding a value of type {name}`α` in a SQLite query.
+Provides a canonical means of binding a non-null value of type {name}`α` in a SQLite query.
 -/
-public class ToSQLite (α : Type u) where
+public class QueryParam (α : Type u) where
   bind : Stmt → Int32 → α → IO Unit
 
-public instance : ToSQLite String where
+public instance : QueryParam String where
   bind := Stmt.bindText
 
-public instance : ToSQLite ByteArray where
+public instance : QueryParam ByteArray where
   bind := Stmt.bindBlob
 
-public instance : ToSQLite Int32 where
+public instance : QueryParam Int32 where
   bind := Stmt.bindInt32
 
-public instance : ToSQLite Int64 where
+public instance : QueryParam Int64 where
   bind := Stmt.bindInt64
 
-public instance : ToSQLite Float where
+public instance : QueryParam Float where
   bind := Stmt.bindFloat
 
-public instance : ToSQLite Unit where
+public instance : QueryParam Unit where
   bind stmt index _ := Stmt.bindNull stmt index
 
-public instance : ToSQLite Nat where
+public instance : QueryParam Nat where
   bind stmt i n := Stmt.bindText stmt i (toString n)
+
+/--
+Provides a canonical means of binding a potentially null value of type {name}`α` in a SQLite query.
+-/
+public class NullableQueryParam (α : Type u) where
+  bind : Stmt → Int32 → α → IO Unit
+
+public instance [QueryParam α] : NullableQueryParam α where
+  bind stmt i val := QueryParam.bind stmt i val
+
+public instance [QueryParam α] : NullableQueryParam (Option α) where
+  bind stmt i
+    | none => stmt.bindNull i
+    | some val => QueryParam.bind stmt i val
 
 end Interpolation
 
 /--
-Provides a canonical way to read a value of type {name}`α` from a SQL query.
+Provides a canonical way to read a non-null value of type {name}`α` from a SQL query.
 -/
-public class Field (α : Type) where
+public class ResultColumn (α : Type) where
   get (stmt : Stmt) (column : Int32) : IO α
 
--- Reading a field can trigger type conversion, so this pattern is not generally safe. Null stays
--- null, at least, so this one is OK.
-public instance [Field α] : Field (Option α) where
+public instance [ResultColumn α] : ResultColumn (Option α) where
   get stmt i := do
     if (← stmt.columnType i) == .null then
       return none
-    else some <$> Field.get stmt i
+    else some <$> ResultColumn.get stmt i
 
-public instance : Field String where
+public instance : ResultColumn String where
   get stmt i := Stmt.columnText stmt i
 
-public instance : Field ByteArray where
+public instance : ResultColumn ByteArray where
   get := Stmt.columnBlob
 
-public instance : Field Float where
+public instance : ResultColumn Float where
   get := Stmt.columnDouble
 
-public instance : Field Int32 where
+public instance : ResultColumn Int32 where
   get := Stmt.columnInt
 
-public instance : Field Int64 where
+public instance : ResultColumn Int64 where
   get stmt i := Stmt.columnInt64 stmt i
 
-public instance : Field Nat where
+public instance : ResultColumn Nat where
   get stmt i := (·.toNatClampNeg) <$> Stmt.columnInt64 stmt i
+
+/--
+Provides a canonical way to read a potentially-null value of type {name}`α` from a SQL query.
+-/
+public class NullableResultColumn (α : Type) where
+  get (stmt : Stmt) (column : Int32) : IO α
+
+public instance [ResultColumn α] : NullableResultColumn α where
+  get := ResultColumn.get
+
+-- Reading a field can trigger type conversion, so this pattern is not generally safe. Null stays
+-- null, at least, so this one is OK.
+public instance [ResultColumn α] : NullableResultColumn (Option α) where
+  get stmt i := do
+    if (← stmt.columnType i) == .null then
+      return none
+    else some <$> ResultColumn.get stmt i
 
 /--
 A monad for reading a row of data from a query result.
@@ -86,10 +115,10 @@ public abbrev RowReader (α : Type) := ReaderT Stmt (StateRefT Int32 IO) α
 namespace RowReader
 
 /--
-Reads the next column using the {lean}`Field α` instance, updating the column counter.
+Reads the next column using the {lean}`ResultColumn α` instance, updating the column counter.
 -/
-public def field [Field α] : RowReader α := do
-  let v ← Field.get (← read) (← getThe Int32)
+public def field [ResultColumn α] : RowReader α := do
+  let v ← ResultColumn.get (← read) (← getThe Int32)
   modify (· + 1)
   return v
 
@@ -111,7 +140,7 @@ A type that can be deserialized from a complete row in a SQL query.
 public class Row (α : Type) where
   read : RowReader α
 
-public instance [Field α] : Row α where
+public instance [ResultColumn α] : Row α where
   read := RowReader.field
 
 public instance [Row α] [Row β] : Row (α × β) where
@@ -197,7 +226,7 @@ macro_rules
         sqlStx ← `($sqlStx ++ $(quote lit))
       else
         let index := args.size + 1
-        args := args.push (← ``(ToSQLite.bind $stmtName (Nat.toInt32 $(quote index)) $(⟨c⟩)))
+        args := args.push (← ``(NullableQueryParam.bind $stmtName (Nat.toInt32 $(quote index)) $(⟨c⟩)))
         sqlStx ← `($sqlStx ++ $(quote s!"?{index}"))
 
     let stx ← `(((let db := $db; SQLite.prepare db $sqlStx >>= fun $stmtName =>
